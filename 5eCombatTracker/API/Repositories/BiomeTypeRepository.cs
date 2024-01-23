d6b1b6c12a8cea14ada27017835d54cd1c5cf7c4
Harrison <harrison.amyotte@gmail.com>
2024-01-23 14:08:14 -0600

    XML comments

--- 5eCombatTracker/API/Repositories/BiomeTypeRepository.cs
+++ 5eCombatTracker/API/Repositories/BiomeTypeRepository.cs
@@ -1,21 +1,33 @@
 ﻿using _5eCombatTracker.API.Interfaces.Repositories;
-using _5eCombatTracker.API.Interfaces.Services;
 using _5eCombatTracker.Data.Helpers;
 using _5eCombatTracker.Data.Models;
-using AutoMapper;
-using AutoMapper.QueryableExtensions;
 using Microsoft.EntityFrameworkCore;
 
 namespace _5eCombatTracker.API.Repositories
 {
+    /// <summary>
+    /// Repository layer for BiomeType, has EF queries to postgress
+    /// </summary>
     public class BiomeTypeRepository : IBiomeTypeRepository
     {
+        /// <summary>
+        /// Context for EF
+        /// </summary>
         public DataContext _dataContext;
+
+        /// <summary>
+        /// Initializes context for EF
+        /// </summary>
+        /// <param name="dataContext"></param>
         public BiomeTypeRepository(DataContext dataContext)
         {
             _dataContext = dataContext;
         }
 
+        /// <summary>
+        /// Grabs all biome types
+        /// </summary>
+        /// <returns>Biome Type model</returns>
         public async Task<List<BiomeType>> GetAllBiomeTypesAsync()
         {
             List<BiomeType> biomeTypes = await _dataContext.BiomeTypes
